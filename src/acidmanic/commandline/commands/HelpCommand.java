5fbebe9914c67a0c66bc811be46c88d5453cc3b6
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2018-06-20 18:06:45 +0430

    Update Help Command to use help generator

--- src/acidmanic/commandline/commands/HelpCommand.java
+++ src/acidmanic/commandline/commands/HelpCommand.java
@@ -1,29 +1,29 @@
 //
 // Translated by CS2J (http://www.cs2j.com): 4/28/2016 9:15:43 PM
 //
-
 package acidmanic.commandline.commands;
 
 import acidmanic.commandline.application.Console;
+import acidmanic.commandline.utility.HelpGenerator;
 
-public class HelpCommand  extends CommandBase 
-{
+public class HelpCommand extends CommandBase {
+
     @Override
     public void execute() {
-        Console.Write(this.creatorFactory.getCommandsHelp());
+        HelpGenerator generator
+                = new HelpGenerator(ApplicationWideTypeRegistery.makeInstance());
+        Console.Write(generator.generateHelp());
         Console.WriteLine("");
     }
 
     @Override
-    public String getdescription()  {
+    public String getdescription() {
         return "Shows This Help.";
     }
 
     @Override
-    public String getName(){
+    public String getName() {
         return "Help";
     }
 
 }
-
-
