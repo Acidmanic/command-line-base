d4913a72d2a63c63e248ce1e4cca1c93fbd7c7c1
Mani <goj_loj@yahoo.com>
2018-07-28 02:40:46 +0430

    Add Command Validation result

---
+++ src/acidmanic/commandline/commands/CommandValidationResult.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acidmanic.commandline.commands;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public class CommandValidationResult {
+    private boolean  isValid;
+    private int numberOfPickedArguments;
+
+    public CommandValidationResult() {
+    }
+
+    public CommandValidationResult(boolean isValid, int numberOfPickedArguments) {
+        this.isValid = isValid;
+        this.numberOfPickedArguments = numberOfPickedArguments;
+    }
+
+    public boolean isIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    public int getNumberOfPickedArguments() {
+        return numberOfPickedArguments;
+    }
+
+    public void setNumberOfPickedArguments(int numberOfPickedArguments) {
+        this.numberOfPickedArguments = numberOfPickedArguments;
+    }
+    
+    
+    
+    
+}
