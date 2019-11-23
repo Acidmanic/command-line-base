4c4cb5cb9df3c0aa183c68816c3885346dd5e9be
Mani Moayedi <acidmanic.moayedi@gmail.com>
2019-11-23 11:56:29 +0330

    Add ParameterBuilder

---
+++ src/acidmanic/commandline/commands/parameters/ParameterBuilder.java
@@ -0,0 +1,72 @@
+package acidmanic.commandline.commands.parameters;
+
+
+public class ParameterBuilder {
+
+
+
+    private String name;
+    private String description;
+    private Class<?> type;
+    private boolean isMandatory;
+    private int index;
+
+    public ParameterBuilder named(String name){
+
+        this.name = name;
+
+        return this;
+    }
+
+
+    public ParameterBuilder described(String description){
+
+        this.description = description;
+
+        return this;
+    }
+
+    public ParameterBuilder ofType(Class<?> type){
+
+        this.type = type;
+
+        return this;
+    }
+
+    
+    public ParameterBuilder mandatory(){
+
+        this.isMandatory = true;
+
+        return this;
+    }
+
+    public ParameterBuilder optional(){
+
+        this.isMandatory = false;
+
+        return this;
+    }
+
+    public ParameterBuilder indexAt(int index){
+
+        this.index = index;
+
+        return this;
+    }
+
+    public <T> Parameter<T> build(){
+        
+        ParameterBase<T> ret;
+
+        if(this.isMandatory){
+            ret = new MandatoryParameter<T>(this.name, this.index,(Class<T>)this.type);
+        }else{
+            ret= new OptionalParameter<T>(this.name, (Class<T>)this.type);
+        }
+
+        ret.setDescription(this.description);
+
+        return ret;
+    }
+}
