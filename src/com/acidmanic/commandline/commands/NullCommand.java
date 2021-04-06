e76c3dd26f6e1ffe0ca35575326d077e1f1ae232
Mani Moayedi <you@example.com>
2021-04-07 02:23:39 +0430

    Fix null command

--- src/com/acidmanic/commandline/commands/NullCommand.java
+++ src/com/acidmanic/commandline/commands/NullCommand.java
@@ -5,39 +5,68 @@
  */
 package com.acidmanic.commandline.commands;
 
+import com.acidmanic.commandline.commands.context.ExecutionContext;
+import com.acidmanic.lightweight.logger.ConsoleLogger;
+import com.acidmanic.lightweight.logger.Logger;
+
 /**
  *
  * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
  */
-public class NullCommand extends CommandBase {
+public class NullCommand implements Command {
 
+    private Logger logger = new ConsoleLogger();
+    private CommandFactory factory = null;
+    
     @Override
-    public void execute() {
-        log("No Such Command!");
+    public void execute(String[] args) {
+        this.logger.log("No such command.");
     }
 
     @Override
-    public String getHelpDescription() {
-        return "No Such Command!";
+    public boolean accepts(String name) {
+        return false;
     }
 
     @Override
-    public String getName() {
-        return "*";
+    public boolean hasArguments() {
+        return false;
     }
 
     @Override
-    public void setCreatorFactory(CommandFactory factory) {
-    }
-
-    @Override
-    public CommandFactory getCreatorFactory() {
+    public String getArgSplitRegEx() {
         return null;
     }
 
     @Override
-    protected String getUsageDescription() {
+    public String getHelpDescription() {
         return "";
     }
+
+    @Override
+    public void setCreatorFactory(CommandFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public CommandFactory getCreatorFactory() {
+        return this.factory;
+    }
+
+    @Override
+    public boolean isVisible() {
+        return false;
+    }
+
+    @Override
+    public void setLogger(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Override
+    public void setContext(ExecutionContext context) {    }
+
+    
+
 
 }
